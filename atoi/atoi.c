01867efbc591fef56833e3d3cd88932c8427ab91
HabibMollah <habib.prodev@gmail.com>
2024-03-22 15:03:22 +0600

    solve recursive atoi

---
+++ atoi/atoi.c
@@ -0,0 +1,35 @@
+#include "../cs50.h"
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    // TODO
+    int length = strlen(input);
+    if (length < 1)
+        return 0;
+    int last_item = input[length - 1] - '0';
+    input[length - 1] = '\0';
+    return convert(input) * 10 + last_item;
+}
