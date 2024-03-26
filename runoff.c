7c9759bff30b0c69a1340487e912e57cf9358d36
HabibMollah <habib.prodev@gmail.com>
2024-03-27 03:57:35 +0600

    implement tabulate function

--- runoff.c
+++ runoff.c
@@ -144,6 +144,18 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            int candidate_index = preferences[i][j];
+            if (!candidates[candidate_index].eliminated)
+            {
+                candidates[candidate_index].votes++;
+                break;
+            }
+        }
+    }
     return;
 }
 
