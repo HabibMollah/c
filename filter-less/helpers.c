15303233f8699dddeb9bba9ce27bbeb6ab53e175
HabibMollah <habib.prodev@gmail.com>
2024-05-17 17:45:31 +0600

    solve grayscale filter

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,8 +1,30 @@
 #include "helpers.h"
+#include <stdio.h>
+
+#define SHADE_COUNT 256
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    BYTE shades_of_gray[SHADE_COUNT];
+    BYTE shade = 0x00;
+    for (int i = 0; i < SHADE_COUNT; i++)
+    {
+        shades_of_gray[i] = shade;
+        shade++;
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE average_of_rgb = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            image[i][j].rgbtRed = shades_of_gray[average_of_rgb];
+            image[i][j].rgbtGreen = shades_of_gray[average_of_rgb];
+            image[i][j].rgbtBlue = shades_of_gray[average_of_rgb];
+        }
+    }
+
     return;
 }
 
